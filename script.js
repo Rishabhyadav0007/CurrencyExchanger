595939d5a1747b1c29ea1ede338b905f5216150d
Rishabh Yadav <nitinrishabhy@gmail.com>
2024-02-08 19:21:12 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,208 @@
+const countryList = {
+  AED: "AE",
+  AFN: "AF",
+  XCD: "AG",
+  ALL: "AL",
+  AMD: "AM",
+  ANG: "AN",
+  AOA: "AO",
+  AQD: "AQ",
+  ARS: "AR",
+  AUD: "AU",
+  AZN: "AZ",
+  BAM: "BA",
+  BBD: "BB",
+  BDT: "BD",
+  XOF: "BE",
+  BGN: "BG",
+  BHD: "BH",
+  BIF: "BI",
+  BMD: "BM",
+  BND: "BN",
+  BOB: "BO",
+  BRL: "BR",
+  BSD: "BS",
+  NOK: "BV",
+  BWP: "BW",
+  BYR: "BY",
+  BZD: "BZ",
+  CAD: "CA",
+  CDF: "CD",
+  XAF: "CF",
+  CHF: "CH",
+  CLP: "CL",
+  CNY: "CN",
+  COP: "CO",
+  CRC: "CR",
+  CUP: "CU",
+  CVE: "CV",
+  CYP: "CY",
+  CZK: "CZ",
+  DJF: "DJ",
+  DKK: "DK",
+  DOP: "DO",
+  DZD: "DZ",
+  ECS: "EC",
+  EEK: "EE",
+  EGP: "EG",
+  ETB: "ET",
+  EUR: "FR",
+  FJD: "FJ",
+  FKP: "FK",
+  GBP: "GB",
+  GEL: "GE",
+  GGP: "GG",
+  GHS: "GH",
+  GIP: "GI",
+  GMD: "GM",
+  GNF: "GN",
+  GTQ: "GT",
+  GYD: "GY",
+  HKD: "HK",
+  HNL: "HN",
+  HRK: "HR",
+  HTG: "HT",
+  HUF: "HU",
+  IDR: "ID",
+  ILS: "IL",
+  INR: "IN",
+  IQD: "IQ",
+  IRR: "IR",
+  ISK: "IS",
+  JMD: "JM",
+  JOD: "JO",
+  JPY: "JP",
+  KES: "KE",
+  KGS: "KG",
+  KHR: "KH",
+  KMF: "KM",
+  KPW: "KP",
+  KRW: "KR",
+  KWD: "KW",
+  KYD: "KY",
+  KZT: "KZ",
+  LAK: "LA",
+  LBP: "LB",
+  LKR: "LK",
+  LRD: "LR",
+  LSL: "LS",
+  LTL: "LT",
+  LVL: "LV",
+  LYD: "LY",
+  MAD: "MA",
+  MDL: "MD",
+  MGA: "MG",
+  MKD: "MK",
+  MMK: "MM",
+  MNT: "MN",
+  MOP: "MO",
+  MRO: "MR",
+  MTL: "MT",
+  MUR: "MU",
+  MVR: "MV",
+  MWK: "MW",
+  MXN: "MX",
+  MYR: "MY",
+  MZN: "MZ",
+  NAD: "NA",
+  XPF: "NC",
+  NGN: "NG",
+  NIO: "NI",
+  NPR: "NP",
+  NZD: "NZ",
+  OMR: "OM",
+  PAB: "PA",
+  PEN: "PE",
+  PGK: "PG",
+  PHP: "PH",
+  PKR: "PK",
+  PLN: "PL",
+  PYG: "PY",
+  QAR: "QA",
+  RON: "RO",
+  RSD: "RS",
+  RUB: "RU",
+  RWF: "RW",
+  SAR: "SA",
+  SBD: "SB",
+  SCR: "SC",
+  SDG: "SD",
+  SEK: "SE",
+  SGD: "SG",
+  SKK: "SK",
+  SLL: "SL",
+  SOS: "SO",
+  SRD: "SR",
+  STD: "ST",
+  SVC: "SV",
+  SYP: "SY",
+  SZL: "SZ",
+  THB: "TH",
+  TJS: "TJ",
+  TMT: "TM",
+  TND: "TN",
+  TOP: "TO",
+  TRY: "TR",
+  TTD: "TT",
+  TWD: "TW",
+  TZS: "TZ",
+  UAH: "UA",
+  UGX: "UG",
+  USD: "US",
+  UYU: "UY",
+  UZS: "UZ",
+  VEF: "VE",
+  VND: "VN",
+  VUV: "VU",
+  YER: "YE",
+  ZAR: "ZA",
+  ZMK: "ZM",
+  ZWD: "ZW",
+};
+
+let BASE_URL =
+  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
+
+let countrySelectionDropDown = document.getElementsByClassName("selection");
+
+for (let select of countrySelectionDropDown) {
+  for (currCode in countryList) {
+    let option = document.createElement("option");
+    option.innerText = currCode;
+    option.value = currCode;
+    if (select.name === "from" && currCode === "INR") {
+      option.selected = "selected";
+    } else if (select.name === "to" && currCode === "USD") {
+      option.selected = "selected";
+    }
+    select.appendChild(option);
+  }
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+const updateFlag = (targetElement) => {
+  let currCode = targetElement.value;
+  let countryCode = countryList[currCode];
+  let image = targetElement.parentElement.querySelector("img");
+  image.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+};
+
+const updateExchangeRate = async () => {
+  let from = document.querySelector("#from").value;
+  let to = document.querySelector("#to").value;
+  let exchangeRateURL = `${BASE_URL}/${from.toLowerCase()}/${to.toLowerCase()}.json`;
+  let response = await fetch(exchangeRateURL);
+  let data = await response.json();
+  let exchangeRate = data[to.toLowerCase()];
+
+  let inputAmount = document.querySelector("#fromInput").value;
+  let calcAmount = parseFloat(inputAmount * exchangeRate).toFixed(2);
+  let outputAmount = document.querySelector("#toInput");
+  outputAmount.value = calcAmount;
+};
+
+document.getElementById("conversionbutton").addEventListener("click", () => {
+  updateExchangeRate();
+});
